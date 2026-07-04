ca7f911c62356f60384785f48eb38a2d911b30b6
MaCaReNo26 <suarezalex833@gmail.com>
2026-07-04 18:21:34 -0500

    Mejora diseño mas imagenes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("imagenInput");
 const preview = document.getElementById("preview");
+const textoPreview = document.getElementById("textoPreview");
 const resultado = document.getElementById("resultado");
 
 input.addEventListener("change", () => {
@@ -8,7 +9,16 @@ input.addEventListener("change", () => {
   if (archivo) {
     preview.src = URL.createObjectURL(archivo);
     preview.classList.remove("oculto");
-    resultado.innerText = "Imagen cargada. Presiona predecir.";
+    textoPreview.style.display = "none";
+
+    resultado.innerHTML = `
+      <div class="estrella">🐾</div>
+      <div>
+        <strong>Imagen cargada</strong>
+        <p>Lista para analizar</p>
+        <span>Presiona el botón para predecir</span>
+      </div>
+    `;
   }
 });
 
@@ -21,7 +31,14 @@ async function enviarImagen() {
   const formData = new FormData();
   formData.append("file", input.files[0]);
 
-  resultado.innerText = "Analizando imagen...";
+  resultado.innerHTML = `
+    <div class="estrella">?</div>
+    <div>
+      <strong>Analizando...</strong>
+      <p>La IA está revisando la imagen</p>
+      <span>Espera un momento</span>
+    </div>
+  `;
 
   try {
     const respuesta = await fetch("http://127.0.0.1:8000/predecir/", {
@@ -32,13 +49,22 @@ async function enviarImagen() {
     const datos = await respuesta.json();
 
     resultado.innerHTML = `
-      Mascota predicha: <br>
-      🐾 ${datos.clase_predicha} <br>
-      Confianza: ${datos.confianza}%
+      <div>
+        <strong>${datos.clase_predicha}</strong>
+        <p>Raza o categoría predicha</p>
+        <span>Confianza: ${datos.confianza}%</span>
+      </div>
     `;
 
   } catch (error) {
     console.error(error);
-    resultado.innerText = "No se pudo conectar con la API.";
+
+    resultado.innerHTML = `
+      <div>
+        <strong>Error</strong>
+        <p>No se pudo conectar con la API</p>
+        <span>Activa el backend con FastAPI</span>
+      </div>
+    `;
   }
 }
